a364df77eb7098d2bc067b90591460563d577859
gretushka <duridom@sibmail.com>
2020-03-18 17:24:50 +0700

    init

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -11,24 +11,38 @@ counter_click = Counter()
 
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    ab_test=request.GET.get('from-landing','noname')
+    if ab_test == 'original':
+        counter_click[1]+=1
+    else:
+        if ab_test == 'test':
+            counter_click[2]+=1
     return render_to_response('index.html')
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
+    ab_test=request.GET.get('ab_test_arg','noname')
+    if ab_test == 'original':
+        counter_show[1]+=1
+    else:
+        if ab_test == 'test':
+            counter_show[2]+=1
+            return render_to_response('landing_alternate.html')
     return render_to_response('landing.html')
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Чтобы отличить с какой версии лендинга был переход
-    # проверяйте GET параметр marker который может принимать значения test и original
-    # Для вывода результат передайте в следующем формате:
+    if counter_show[1] == 0:
+        stat_original = 0
+    else:
+        stat_original=counter_click[1]/counter_show[1]
+
+    if counter_show[2] == 0:
+        stat_test = 0
+    else:
+        stat_test=counter_click[2]/counter_show[2]
+
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': stat_test,
+        'original_conversion': stat_original,
     })
--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+import csv
+import urllib.parse
 
 
 def index(request):
     return redirect(reverse(bus_stations))
 
 
+
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+    bus_stations = []
+    with open('data-398-2018-08-30.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            bus_stations.append({'Name': row['Name'], 'Street': row['Street'], 'District': row['District']})
+    current_page_num = int(request.GET.get('page',1))
+    paginator=Paginator(bus_stations,20,4)
+    current_page=paginator.page(current_page_num)
+    if current_page.has_next():
+        params = urllib.parse.urlencode({'page': current_page_num + 1})
+        next_page_url = "?%s" % params
+    else:
+        next_page_url = None
+    if current_page.has_previous():
+        params = urllib.parse.urlencode({'page': current_page_num - 1})
+        prev_page_url = "?%s" % params
+    else:
+        prev_page_url = None
     return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
+        'bus_stations': current_page.object_list,
         'current_page': current_page,
-        'prev_page_url': None,
+        'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
 
